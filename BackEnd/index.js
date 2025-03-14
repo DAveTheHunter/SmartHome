922d3e9a84d2b6563b1a14b8c8da7f3d3b9eb62a
Dawit Biniyam <dawetbiniyame@gmail.com>
2025-03-14 07:40:30 +0300

    Going good

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,18 +6,23 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 //for the leackage detection 
-app.post('/event/leackage_alert', async(res, req, next) => {
+app.post('/event/leackage_alert/message/Water leackage detected', async(res, req, next) => {
+    const data = req.body
     res.send(data)
 })
 //for the motion
-app.post('/event/motion_detected', async(res, req, next) => {
+app.post('/event/motion_detected/message/motiondetected', async(res, req, next) => {
+    const data = req.body;
     res.send(data)
 })
-//
-app.post('/event/', async(res, req, next) => {
+//controll the door
+app.post('/event/door_status', async(res, req, next) => {
     res.send(data)
 })
-
+//bloakage detaction 
+app.post('/event/bloackage_alert', async(res, req, next) => {
+    res.send(data)
+})
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
